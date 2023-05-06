cfb9fb55ea41b1b3f46dbfddfc694ce3a26dfa13
Pedro <pedro.enrique1599@icloud.com>
2023-05-06 05:23:24 -0300

    IOException

--- csharp-exceptions-main/csharp_exception-main/csharp_exception/Program.cs
+++ csharp-exceptions-main/csharp_exception-main/csharp_exception/Program.cs
@@ -1,23 +1,36 @@
-﻿using csharp_exception;
+﻿using ByteBank;
+using csharp_exception;
 using csharp_exception.Contas;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
-    //conta1.Sacar(50);
-    //Console.WriteLine("Saldo: $" + conta1.GetSaldo() + ".");
-    //conta1.Sacar(150);
-    //Console.WriteLine("Saldo: $" + conta1.GetSaldo() + ".");
+    LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+    leitor.Fechar();
 }
-catch(ArgumentException ex)
+catch (IOException)
 {
-    Console.WriteLine("Parâmetro com erro: " + ex.ParamName + ".");
-    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
-    Console.WriteLine(ex.StackTrace);
-    Console.WriteLine(ex.Message);
+    Console.WriteLine("Leitura de arquivo interrompida!");
 }
-catch(SaldoInsufucienteException ex)
-{
-    Console.WriteLine("Operação negada! Saldo Insuficiente!");
-    Console.WriteLine(ex.Message);
-}
+
+//try
+//{
+//    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+//    //conta1.Sacar(50);
+//    //Console.WriteLine("Saldo: $" + conta1.GetSaldo() + ".");
+//    //conta1.Sacar(150);
+//    //Console.WriteLine("Saldo: $" + conta1.GetSaldo() + ".");
+//}
+//catch(ArgumentException ex)
+//{
+//    Console.WriteLine("Parâmetro com erro: " + ex.ParamName + ".");
+//    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+//    Console.WriteLine(ex.StackTrace);
+//    Console.WriteLine(ex.Message);
+//}
+//catch(SaldoInsufucienteException ex)
+//{
+//    Console.WriteLine("Operação negada! Saldo Insuficiente!");
+//    Console.WriteLine(ex.Message);
+//}
