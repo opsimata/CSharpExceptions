2c310622ceb390aae7b1e3eb0227693418ec46c7
Pedro <pedro.enrique1599@icloud.com>
2023-05-06 05:06:28 -0300

    Inner Exception

--- csharp-exceptions-main/csharp_exception-main/csharp_exception/SaldoInsufucienteException.cs
+++ csharp-exceptions-main/csharp_exception-main/csharp_exception/SaldoInsufucienteException.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp_exception
+﻿namespace csharp_exception
 {
-    public class SaldoInsufucienteException : Exception
+    public class SaldoInsufucienteException : OperacaoFinanceiraException
     {
-        public SaldoInsufucienteException(string mensagem) : base(mensagem) 
+        public SaldoInsufucienteException(string mensagem) : base(mensagem)
         {
 
         }
